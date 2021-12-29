960113e33805db48428e61e2b7f90a9034effca2
choijeaho86@gmail.com <choijeaho86@gmail.com>
2021-12-29 16:36:26 +0900

    postRequest

--- src/rest-api/rest-api.controller.ts
+++ src/rest-api/rest-api.controller.ts
@@ -1,19 +1,15 @@
 // 경로 : src/rest-api.controller.ts
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { GetRestApiDto } from './dto/get-rest-api.dto';
 import { RestApiService } from './rest-api.service';
 @ApiTags('Axios')
 @Controller('rest-api')
 export class RestApiController {
   constructor(private readonly restApiService: RestApiService) {}
 
-  @Get()
-  findAll() {
-    return this.restApiService.getRequest();
+  @Post()
+  create(@Body() createRestApiDto: GetRestApiDto) {
+    return this.restApiService.postRequest(createRestApiDto);
   }
 }
-
-// @Post()
-// create(@Body() createRestApiDto: GetRestApiDto) {
-//   return this.restApiService.create(createRestApiDto);
-// }
--- src/rest-api/rest-api.service.ts
+++ src/rest-api/rest-api.service.ts
@@ -3,23 +3,25 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
 import { map, Observable } from 'rxjs';
+import { GetRestApiDto } from './dto/get-rest-api.dto';
 
 @Injectable()
 export class RestApiService {
   constructor(private httpService: HttpService) {}
 
-  create() {
-    console.log('Start Get request');
-    return 'This action adds a new restApi';
-  }
-
-  getRequest(): Observable<AxiosResponse<Body>> {
-    return this.httpService.get('http://localhost:4000').pipe(
-      map((res) => {
-        console.log('response : ', res);
-        console.log('response(data) : ', res.data);
-        return res.data;
-      }),
-    );
+  postRequest(dto: GetRestApiDto): Observable<AxiosResponse<Body>> {
+    // dto는 비어있기 때문에 Swagger에서 Object를 임의로 넣으면 됩니다.
+    console.log('Start Post request');
+    return this.httpService
+      .post('http://localhost:4000', {
+        ...dto,
+      })
+      .pipe(
+        map((res) => {
+          console.log('response : ', res);
+          console.log('response(data) : ', res.data);
+          return res.data;
+        }),
+      );
   }
 }
